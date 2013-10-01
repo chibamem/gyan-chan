2251f7f7734b489a835b2eabb9df66ac81967b47
Keisuke Chiba <k-chiba@esm.co.jp>
2013-10-01 15:34:11 +0900

    add blight

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 $(function() {
   setInterval(function() {
-    $("img[src^='https://secure.gravatar.com/avatar/fd2bda3f1aa0c7b0d03b570f81fea9e2']").attr('src', 'http://www2.hi-nobori.net/gundam_icon/ico2/ms15@48.gif');
+    gyan();
+    blight();
   }, 10000);
+
+  function gyan() {
+    $("img[src^='https://secure.gravatar.com/avatar/fd2bda3f1aa0c7b0d03b570f81fea9e2']").attr('src', 'http://www2.hi-nobori.net/gundam_icon/ico2/ms15@48.gif');
+  }
+
+  function blight() {
+    $("img[src^='https://secure.gravatar.com/avatar/5ed3e1702c476bd4d699614a9f71e9f1?s=42&d=mm").attr('src', 'http://t2.gstatic.com/images?q=tbn:ANd9GcQl91LBIgwemdJ12hOhBVM4YdfyTGh_C-0dNyKLzamiEZd7BFfPNA')
+  }
 });
