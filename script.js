de933f9eadd7b08347f523301c0cc8f934644f85
Keisuke Chiba <k-chiba@esm.co.jp>
2013-10-01 15:19:19 +0900

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+$(function() {
+  setInterval(function() {
+    $("img[src^='https://secure.gravatar.com/avatar/fd2bda3f1aa0c7b0d03b570f81fea9e2']").attr('src', 'http://www2.hi-nobori.net/gundam_icon/ico2/ms15@48.gif');
+  }, 10000);
+});
